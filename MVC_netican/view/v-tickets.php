f50bf23b369c17711d6dd747b591ae4e67a39617
Sididboo <theovacant@gmail.com>
2020-10-26 11:02:51 +0100

    Ajout du lien vers bootstrap par CDN

--- MVC_netican/view/v-tickets.php
+++ MVC_netican/view/v-tickets.php
@@ -7,8 +7,7 @@
 
     <!--CSS-->
     <link rel="stylesheet" href="./habillage/styles/css-header.css">
-    <link rel="stylesheet" href="./habillage/lib/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./habillage/styles/global.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <!--JS-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
