357a155e3262dfc7fa88bcfd9676bb17f56c5ea0
Skollid <mangarom1@hotmail.fr>
2020-11-03 10:47:32 +0100

    modifs

--- MVC_netican/controller/a-articles_codeBarre.php
+++ MVC_netican/controller/a-articles_codeBarre.php
@@ -4,12 +4,12 @@
     $leProduit = new Produits();
     $leProduit->retrieveByCodeBarre($_REQUEST['code']);
 
-    echo $leProduit->get_leIngredient()->get_laSousCategorie()->get_laCategorie()->get_nom();
-    echo $leProduit->get_leIngredient()->get_laSousCategorie()->get_nom();
-    echo $leProduit->get_leIngredient()->get_nom();
-    echo $leProduit->get_laMarque()->get_nom();
-    echo $leProduit->get_lePays()->get_nom();
-    echo $leProduit->get_leTypeConditionnement()->get_nom();
+    $dataProduit = "$leProduit->get_leIngredient()->get_laSousCategorie()->get_laCategorie()->get_nom();";
+    $dataProduit .= "$leProduit->get_leIngredient()->get_laSousCategorie()->get_nom();";
+    $dataProduit .= "$leProduit->get_leIngredient()->get_nom();";
+    $dataProduit .= "$leProduit->get_laMarque()->get_nom();";
+    $dataProduit .= "$leProduit->get_lePays()->get_nom();";
+    $dataProduit .= "$leProduit->get_leTypeConditionnement()->get_nom();";
     echo $leProduit->get_quantiteConditionnement();
     echo $leProduit->get_laUnite()->get_nom();
 
